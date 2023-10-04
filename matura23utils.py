7de9da47bfd65a4fac0ceaa67a448b6e2dd133db
adrigit04 <adrianovkvk@gmail.com>
2023-10-05 01:00:47 +0200

    dogonearfruit mit Rückwärtskorrektur

--- matura23utils.py
+++ matura23utils.py
@@ -17,54 +17,9 @@ class Matura23Utils(object):
         4 : 'passionsfrucht'
     }
 
-
-
     sleepSeconds = 0.5
 
-    @staticmethod        
-    def getDetectedObjectInfoList(img,results,cameraWidth,cameraHeight):
-        objectInfoList = []
-
-         # in results ist die Liste der erkannten Objekte
-        if (len(results) > 0 and results[0] is not None):
-
-            # wir gehen über alle Elemente in der Liste und erstellen ein Objekt mit den Koordinaten und anderen Infos
-            for i in range(len(results)):
-                eachObjectInfo = {}
-                eachObject = results[i]
-                # Convert the bounding box figures from relative coordinates
-                # to absolute coordinates based on the original resolution
-                ymin, xmin, ymax, xmax = eachObject['bounding_box']
-                xmin = int(xmin * cameraWidth)
-                xmax = int(xmax * cameraWidth)
-                ymin = int(ymin * cameraHeight)
-                ymax = int(ymax * cameraHeight)
-
-                #print("[xmin: {} | xmax: {} | ymin: {} | ymax {}]".format(xmin,xmax,ymin,ymax))
-
-                # Nullpunkt ist oben links im Quadrat
-                try:
-                    eachObjectInfo['x'] = xmin # x-coordinate
-                    eachObjectInfo['y'] = ymin # y-coordinate
-                    eachObjectInfo['width'] = xmax-xmin # width
-                    eachObjectInfo['height'] = ymax-ymin # height
-                    eachObjectInfo['class_id'] = int(eachObject['class_id'])  # object class_id default: 0
-                    eachObjectInfo['label'] = Matura23Utils.labels_map[eachObject['class_id']]  # object label default 'None'
-                    eachObjectInfo['count'] = len(results) # number found objects default: 0
-                except:
-                    print("somethiing went wrong!")
-
-                # Objektinfo der Liste hinzufügen
-                objectInfoList.append(eachObjectInfo)       
-        # else:
-            # print('no object found')
-
-        return objectInfoList
-
-
-
-
-
+    
     @staticmethod
     def doGoInNewPosition(px,objectInfoList):
         # Neue Suchposition einrichten
@@ -108,7 +63,7 @@ class Matura23Utils(object):
 
         while(True):
 
-            time.sleep(1)
+            time.sleep(0.1)
             img = Vilib.detect_obj_parameter['object_img']
             results = Vilib.detect_obj_parameter['object_results']
             objectInfoList = Matura23Utils.getDetectedObjectInfoList(img, results, cameraWidth, cameraHeight)
@@ -210,16 +165,54 @@ class Matura23Utils(object):
                     lastDirectionX = directionX
 
                 if directionX == 'forward':
-                    px.set_dir_servo_angle(0)
-                    px.forward(velocity) 
-                elif directionX == 'right':
-                    px.set_dir_servo_angle(angleToObject)
-                    px.forward(velocity) 
-                elif directionX == 'left':
-                    px.set_dir_servo_angle(angleToObject)
-                    px.forward(velocity) 
-                else:
+                    # Echosensor ansteuern um Stückweise an Objekt zu gelangen
+                    targetDistance = 20
+                    distance = round(px.ultrasonic.read(), 2)
+                    if distance <= targetDistance:
+                        nearFruit = True
+                        print('in front of object')
+                        break
 
+                    # wenn forwärts gerade aus
+                    Matura23Utils.workaroundSetAngleZero(px)
+                    px.forward(velocity)
+                    time.sleep(0.5)
+                    px.stop()
+
+                    # Echosensor ansteuern um Stückweise an Objekt zu gelangen
+                    targetDistance = 20
+                    distance = round(px.ultrasonic.read(), 2)
+                    if distance <= targetDistance:
+                        nearFruit = True
+                        print('in front of object')
+                        break
+
+
+                elif directionX == 'right':
+                    # lenken nach links ein, weil wir Rückwärts fahren
+                    # danach ein bisschen vorwärts
+                    px.set_dir_servo_angle(-angleToObject) # Weil Rückwärts, müssen wir servo auf andere Seite Richten
+                    px.backward(velocity)
+                    time.sleep(0.2)
+                    px.stop()
+                    Matura23Utils.workaroundSetAngleZero(px)
+                    px.forward(velocity)
+                    time.sleep(0.2)
+                    px.stop()
+
+                elif directionX == 'left':
+                    # lenken nach rechts ein, weil wir Rückwärts fahren
+                    # danach ein bisschen vorwärts
+                    px.set_dir_servo_angle(-angleToObject) # - Weil Rückwärts, müssen wir servo auf andere Seite Richten
+                    px.backward(velocity)
+                    time.sleep(0.2)
+                    px.stop()
+                    Matura23Utils.workaroundSetAngleZero(px)
+                    px.forward(velocity)
+                    time.sleep(0.2)
+                    px.stop()
+
+                else:
                     if lastDirectionX == 'right':
                         px.set_dir_servo_angle(-lastAngleToObject)
                         px.backward(10)
@@ -232,16 +225,10 @@ class Matura23Utils(object):
                 time.sleep(0.5)
                 px.stop()
 
-                # Echosensor ansteuern um Stückweise an Objekt zu gelangen
-                targetDistance = 20
-                
-                distance = round(px.ultrasonic.read(), 2)
-                if distance <= targetDistance:
-                    nearFruit = True
-                    print('in front of object')
 
-                
+                # neues Bild wird ausgelesen
                 found,foundObjectInfo = Matura23Utils.getFoundObjectInfo(cameraWidth,cameraHeight,fruitLabel)
+
         except Exception as e:
             # Handle any type of error and print a custom error message
             print("Error:", e)
@@ -314,6 +301,47 @@ class Matura23Utils(object):
 #########################################################################################
 # Hilfsfunktionen
 #########################################################################################
+
+    @staticmethod        
+    def getDetectedObjectInfoList(img,results,cameraWidth,cameraHeight):
+        objectInfoList = []
+
+            # in results ist die Liste der erkannten Objekte
+        if (len(results) > 0 and results[0] is not None):
+
+            # wir gehen über alle Elemente in der Liste und erstellen ein Objekt mit den Koordinaten und anderen Infos
+            for i in range(len(results)):
+                eachObjectInfo = {}
+                eachObject = results[i]
+                # Convert the bounding box figures from relative coordinates
+                # to absolute coordinates based on the original resolution
+                ymin, xmin, ymax, xmax = eachObject['bounding_box']
+                xmin = int(xmin * cameraWidth)
+                xmax = int(xmax * cameraWidth)
+                ymin = int(ymin * cameraHeight)
+                ymax = int(ymax * cameraHeight)
+
+                #print("[xmin: {} | xmax: {} | ymin: {} | ymax {}]".format(xmin,xmax,ymin,ymax))
+
+                # Nullpunkt ist oben links im Quadrat
+                try:
+                    eachObjectInfo['x'] = xmin # x-coordinate
+                    eachObjectInfo['y'] = ymin # y-coordinate
+                    eachObjectInfo['width'] = xmax-xmin # width
+                    eachObjectInfo['height'] = ymax-ymin # height
+                    eachObjectInfo['class_id'] = int(eachObject['class_id'])  # object class_id default: 0
+                    eachObjectInfo['label'] = Matura23Utils.labels_map[eachObject['class_id']]  # object label default 'None'
+                    eachObjectInfo['count'] = len(results) # number found objects default: 0
+                except:
+                    print("somethiing went wrong!")
+
+                # Objektinfo der Liste hinzufügen
+                objectInfoList.append(eachObjectInfo)       
+        # else:
+            # print('no object found')
+
+        return objectInfoList
+
 
     @staticmethod
     def workaroundSetAngleZero(px):
